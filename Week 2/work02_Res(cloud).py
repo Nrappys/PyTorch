8c8a25c258491a6df607a110475b53ff0d73a62a
Nrappys <keskarn.parn@gmail.com>
2026-02-03 12:54:07 +0700

    Res

---
+++ Week 2/work02_Res(cloud).py
@@ -0,0 +1,187 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from torchvision.models import resnet50
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import os
+import kagglehub
+
+# Load Data
+path = kagglehub.dataset_download("paultimothymooney/chest-xray-pneumonia")
+data_dir = os.path.join(path, "chest_xray")
+
+print("Data path:", data_dir)
+
+#check device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
+# Transforms Data
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.Grayscale(num_output_channels=1),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5], std=[0.5])
+])
+
+train_dataset = datasets.ImageFolder(
+    root=os.path.join(data_dir, "train"),
+    transform=transform
+)
+
+val_dataset = datasets.ImageFolder(
+    root=os.path.join(data_dir, "val"),
+    transform=transform
+)
+
+test_dataset = datasets.ImageFolder(
+    root=os.path.join(data_dir, "test"),
+    transform=transform
+)
+
+print("Classes:", train_dataset.classes)
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+val_loader   = DataLoader(val_dataset, batch_size=64, shuffle=False)
+test_loader  = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+
+# resnet50 Model
+class ResNet50MNIST(nn.Module):
+    def __init__(self):
+        super(ResNet50MNIST, self).__init__()
+        self.model = resnet50(pretrained=True)
+        self.model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.model.fc = nn.Linear(2048,10)
+
+    def forward(self, x):
+        return self.model(x)
+
+# Setting
+model = ResNet50MNIST().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+epochs = 10
+train_loss_list = []
+val_loss_list = []
+val_acc_list = []
+
+# Start Training
+for epoch in range(epochs):
+
+    # ----------------- TRAIN -----------------
+    model.train()
+    train_loss = 0.0
+
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+
+    train_loss /= len(train_loader)
+    train_loss_list.append(train_loss)
+
+    # ----------------- VALIDATION -----------------
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    val_loss /= len(val_loader)
+    val_acc = 100 * correct / total
+
+    val_loss_list.append(val_loss)
+    val_acc_list.append(val_acc)
+
+    print(f"Epoch [{epoch+1}/{epochs}] "
+          f"Train Loss: {train_loss:.4f} | "
+          f"Val Loss: {val_loss:.4f} | "
+          f"Val Acc: {val_acc:.2f}%")
+
+# ----------------- TEST -----------------
+model.eval()
+test_loss = 0.0
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        test_loss += loss.item()
+
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+test_loss /= len(test_loader)
+test_acc = 100 * correct / total
+
+print(f"\nTest Loss: {test_loss:.4f}")
+print(f"Test Accuracy: {test_acc:.2f}%")
+
+# PLOT
+plt.figure(figsize=(8, 5))
+plt.plot(range(1, epochs+1), train_loss_list, label="Train Loss", marker='o')
+plt.plot(range(1, epochs+1), val_loss_list, label="Validation Loss", marker='o')
+
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.title("Training vs Validation Loss")
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+
+y_true = []
+y_pred = []
+
+model.eval()
+with torch.no_grad():
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+
+        y_true.extend(labels.cpu().numpy())
+        y_pred.extend(predicted.cpu().numpy())
+
+cm = confusion_matrix(y_true, y_pred)
+
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=cm,
+    display_labels=train_dataset.classes
+)
+
+plt.figure(figsize=(6, 6))
+disp.plot(cmap=plt.cm.Blues, values_format='d')
+plt.title("Confusion Matrix (Test Set)")
+plt.tight_layout()
+plt.show()
---
+++ Week 2/work02_Res(local).py
@@ -0,0 +1,201 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader, TensorDataset
+from torchvision import datasets, transforms
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from torchvision.models import resnet50
+import os
+
+# Load Data 
+path = r"Week 2\chest_xray"
+print("Data path :", path)
+
+#check device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
+# Transforms Data
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.Grayscale(num_output_channels=1),
+    transforms.ToTensor()
+])
+
+train_dataset = datasets.ImageFolder(
+    root=f"{path}/train",
+    transform=transform
+)
+
+val_dataset = datasets.ImageFolder(
+    root=f"{path}/val",
+    transform=transform
+)
+
+test_dataset = datasets.ImageFolder(
+    root=f"{path}/test",
+    transform=transform
+)
+
+print(train_dataset.classes)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=64,
+    shuffle=True
+)
+
+val_loader = DataLoader(
+    val_dataset,
+    batch_size=64,
+    shuffle=False
+)
+
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=64,
+    shuffle=False
+)
+
+
+# resnet50 Model
+class ResNet50MNIST(nn.Module):
+    def __init__(self):
+        super(ResNet50MNIST, self).__init__()
+        self.model = resnet50(pretrained=True)
+        self.model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.model.fc = nn.Linear(2048,10)
+
+    def forward(self, x):
+        return self.model(x)
+
+# Setting
+model = ResNet50MNIST().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+train_loss_list = []
+val_loss_list = []
+val_acc_list = []
+
+epochs = 10
+
+# Start Training
+for epoch in range(epochs):
+
+    # ===================== TRAIN =====================
+    model.train()
+    running_loss = 0.0
+
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    train_loss = running_loss / len(train_loader)
+    train_loss_list.append(train_loss)
+
+    # ===================== VALIDATION =====================
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    val_loss /= len(val_loader)
+    val_acc = 100 * correct / total
+
+    val_loss_list.append(val_loss)
+    val_acc_list.append(val_acc)
+
+    print(f"Epoch [{epoch+1}/{epochs}] "
+          f"Train Loss: {train_loss:.4f} | "
+          f"Val Loss: {val_loss:.4f} | "
+          f"Val Acc: {val_acc:.2f}%")
+
+# ----------------- TEST -----------------
+model.eval()
+test_loss = 0.0
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        test_loss += loss.item()
+
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+test_loss /= len(test_loader)
+test_acc = 100 * correct / total
+
+print(f"Test Loss: {test_loss:.4f}")
+print(f"Test Accuracy: {test_acc:.2f}%")
+
+# PLOT
+plt.figure(figsize=(8, 5))
+plt.plot(range(1, epochs+1), train_loss_list, label="Train Loss", marker='o')
+plt.plot(range(1, epochs+1), val_loss_list, label="Validation Loss", marker='o')
+
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.title("Training vs Validation Loss")
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+
+y_true = []
+y_pred = []
+
+model.eval()
+with torch.no_grad():
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+
+        y_true.extend(labels.cpu().numpy())
+        y_pred.extend(predicted.cpu().numpy())
+
+cm = confusion_matrix(y_true, y_pred)
+
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=cm,
+    display_labels=train_dataset.classes
+)
+
+plt.figure(figsize=(6, 6))
+disp.plot(cmap=plt.cm.Blues, values_format='d')
+plt.title("Confusion Matrix (Test Set)")
+plt.tight_layout()
+plt.show()
